2c7d1d452c804e5507b016067004b61d3986e422
akhmadrizki <akhmadrizki24@gmail.com>
2021-06-15 23:25:46 +0800

    Add argon resources and make login page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Login Admin
+Route::get('/admin', [LoginController::class, 'index'])->name('admin');
+
+Route::prefix('/')->group(function () {
+    Route::get('/login', function () {
+        return redirect()->to('/admin');
+    })->name('login');
+    Route::post('/login', [LoginController::class, 'login'])->name('login');
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+});
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::prefix('dashboard')->group(function () {
+        Route::get('/wellcome', [DashboardController::class, 'index'])->name('index.wellcome');
+    });
+});
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
